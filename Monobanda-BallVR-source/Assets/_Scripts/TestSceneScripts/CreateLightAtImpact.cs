67f5edc91f5e907ccb9ded15405bc79bf1f46311
Evelyn van den Brand <everlighthelios@gmail.com>
2018-06-07 14:25:16 +0200

    updated door nik op donderdag

---
+++ Monobanda-BallVR-source/Assets/_Scripts/TestSceneScripts/CreateLightAtImpact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLightAtImpact : MonoBehaviour {
+
+	public GameObject lightPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		//GameObject currentLight = 
+		Instantiate(lightPrefab, collision.gameObject.transform.position, Quaternion.identity);
+		//print(collision.gameObject.name);
+		if(collision.gameObject.GetComponent<DestroyAtZeroVelocity>()){
+			collision.gameObject.GetComponent<DestroyAtZeroVelocity>().startTimer = true;
+		}
+	}
+
+	
+
+}

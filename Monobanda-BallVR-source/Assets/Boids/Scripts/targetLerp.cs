9aeb54a9fee7a3c39326f2a925069d66d69108b2
Robbie <robbie.degier@gmail.com>
2018-06-11 15:54:15 +0200

    Target tracking
    Boids target lerp

--- Monobanda-BallVR-source/Assets/Boids/Scripts/targetLerp.cs
+++ Monobanda-BallVR-source/Assets/Boids/Scripts/targetLerp.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class targetLerp : MonoBehaviour {
 
-	public Transform t;
-	
+	public Transform currLoc;
+	public Transform tarLoc;
+	public float spd = 0.3f;
 
-	// Use this for initialization
-	void Start () {
+	private float stt, len;
+
+	void Update () {
+
+
+		float dc = (Time.time - stt) * spd;
+    float fr = dc / len;
+    transform.position = Vector3.Lerp(currLoc.position, tarLoc.position, fr);
 
 	}
 
-	// Update is called once per frame
-	void Update () {
-
+	void getLocation(Transform _t){
+		//
+		if (_t.position != tarLoc.position)
+		{
+			tarLoc.position = _t.position;
+			stt = Time.deltaTime;
+			len = Vector3.Distance(currLoc.position,tarLoc.position);
+		}
 	}
 }

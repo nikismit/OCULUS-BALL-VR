9843e7e2cfcde64db0a466af8ca32c720645da6f
Niki Smit <jijjezin@gmail.com>
2020-12-13 17:30:11 +0100

    laatste commit voor naar HD gooien

--- Monobanda-BallVR-source/Assets/6_Scripts/TestScripts/SwitchSceneOnGoButton.cs
+++ Monobanda-BallVR-source/Assets/6_Scripts/TestScripts/SwitchSceneOnGoButton.cs
@@ -15,7 +15,7 @@ public class SwitchSceneOnGoButton : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.One)){
+		if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.Space)){
             print("Load new level!");
 			screenfade_Script.FadeToNewLevel(sceneToLoadName);
 		}
